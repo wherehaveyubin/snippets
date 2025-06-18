9b4b94061a9305c60c807ebc8b1a86e1024a9cb4
Yubin Lee <42401542+lucyubin@users.noreply.github.com>
2025-06-18 10:46:39 +0900

    Update deep-learning-neural-network-MNIST.py

--- deep-learning-neural-network-MNIST.py
+++ deep-learning-neural-network-MNIST.py
@@ -7,7 +7,44 @@ import mnist
 importlib.reload(mnist)
 from mnist import *
 
-# https://github.com/lucyubin/snippets/blob/main/deep-learning-load-mnist.py
+"""
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+    """Import MNIST data
+    Parameters
+    ----------
+    normalize : Whether to normalize image pixel values to the range
+        If True, [0.0, 1.0]
+        If Flase, [0.0, 255.0]
+    one_hot_label :
+        If True, labels are returned as one-hot encoded arrays.
+        A one-hot array: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], answer is 1. 
+    flatten : Whether to flatten the input images into 1D arrays.
+
+    Returns
+    -------
+    (train_images, train_labels), (test_images, test_labels)
+    """
+    if not os.path.exists(save_file):
+        init_mnist()
+        
+    with open(save_file, 'rb') as f:
+        dataset = pickle.load(f)
+    
+    if normalize:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].astype(np.float32)
+            dataset[key] /= 255.0
+            
+    if one_hot_label:
+        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])    
+    
+    if not flatten:
+         for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label']) 
+"""
 
 # Import MNIST data
 (x_train, t_train), (x_test, t_test) = \
