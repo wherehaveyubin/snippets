9bb8155958cf4167893f1ab95add5b5b594dd708
Yubin Lee <42401542+wherehaveyubin@users.noreply.github.com>
2025-07-15 12:33:21 +0900

    Create deep-learning-6-CNN.py

---
+++ deep-learning-6-CNN.py
@@ -0,0 +1,326 @@
+"""
+6.4.1 4D array
+"""
+x = np.random.rand(10, 1, 28, 28) 
+x.shape  # (10, 1, 28, 28)
+
+# Accessing the first data sample
+x[0].shape  # (1, 28, 28)
+x[1].shape  # (1, 28, 28)
+
+# Accessing the spatial data of the first channel of the first sample
+x[0, 0]  # or x[0][0]
+
+"""
+6.4.3 Convolutional layer
+"""
+import sys, os
+from common.util import im2col
+
+x1 = np.random.rand(1, 3, 7, 7) # (number of data, number of chanel, height, width)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape) # (9, 75)
+
+x1 = np.random.rand(10, 3, 7, 7) # 10 data
+col1 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape) # (90, 75)
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T # Unfold filter
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+      
+"""
+6.4.4 Pooling layer
+"""
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=2, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+        
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        
+        return dx
+
+"""
+6.5 CNN
+"""
+import sys, os
+import pickle
+import numpy as np
+from collections import OrderedDict
+from common.layers import *
+from common.gradient import numerical_gradient
+
+
+class SimpleConvNet:
+    """Simple Convolutional Neural Network
+    
+    Architecture:
+    conv - relu - pool - affine - relu - affine - softmax
+    
+    Parameters
+    ----------
+    input_size : Input size (784 for MNIST)
+    hidden_size_list : List of neuron counts in each hidden layer (e.g., [100, 100, 100])
+    output_size : Output size (10 for MNIST)
+    activation : Activation function - 'relu' or 'sigmoid'
+    weight_init_std : Standard deviation for weight initialization (e.g., 0.01)
+        If 'relu' or 'he', uses He initialization.
+        If 'sigmoid' or 'xavier', uses Xavier initialization.
+    """
+
+    def __init__(self, input_dim=(1, 28, 28), 
+                 conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+                 hidden_size=100, output_size=10, weight_init_std=0.01):
+        # Extract convolution layer hyperparameters from dictionary
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+
+        # Compute output size of convolution layer
+        conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+
+        # Initialize weights
+        self.params = {}
+        # Weights for the first convolution layer
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        # Biases for the first convolution layer
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * \
+                            np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        # Create layers
+        # Add layers to an OrderedDict in sequence
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
+                                           conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+        
+        # Store final SoftmaxWithLoss layer separately
+        self.last_layer = SoftmaxWithLoss()
+        # End of SimpleConvNet initialization
+
+    # Inference method
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    # Method to calculate loss
+    def loss(self, x, t):
+        """Computes the loss function.
+
+        Parameters
+        ----------
+        x : Input data
+        t : True labels
+        """
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    def accuracy(self, x, t, batch_size=100):
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+        
+        acc = 0.0
+        
+        for i in range(int(x.shape[0] / batch_size)):
+            tx = x[i*batch_size:(i+1)*batch_size]
+            tt = t[i*batch_size:(i+1)*batch_size]
+            y = self.predict(tx)
+            y = np.argmax(y, axis=1)
+            acc += np.sum(y == tt)
+        
+        return acc / x.shape[0]
+
+    # Compute gradients using numerical differentiation
+    def numerical_gradient(self, x, t):
+        """Computes gradients (numerical differentiation).
+
+        Parameters
+        ----------
+        x : Input data
+        t : True labels
+
+        Returns
+        -------
+        grads : Dictionary containing gradients
+            grads['W1'], grads['W2'], ... : Weights of each layer
+            grads['b1'], grads['b2'], ... : Biases of each layer
+        """
+        loss_w = lambda w: self.loss(x, t)
+
+        grads = {}
+        for idx in (1, 2, 3):
+            grads['W' + str(idx)] = numerical_gradient(loss_w, self.params['W' + str(idx)])
+            grads['b' + str(idx)] = numerical_gradient(loss_w, self.params['b' + str(idx)])
+
+        return grads
+
+    def gradient(self, x, t):
+        """Computes gradients (backpropagation).
+
+        Parameters
+        ----------
+        x : Input data
+        t : True labels
+
+        Returns
+        -------
+        grads : Dictionary containing gradients
+            grads['W1'], grads['W2'], ... : Weights of each layer
+            grads['b1'], grads['b2'], ... : Biases of each layer
+        """
+        # Forward pass
+        self.loss(x, t)
+
+        # Backward pass
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # Store results
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name="params.pkl"):
+        with open(os.path.dirname(__file__) + '/' + file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i+1)]
+            self.layers[key].b = self.params['b' + str(i+1)]
+
+import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from common.trainer import Trainer
+
+# Load data
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+# If training takes too long, reduce the dataset size.
+#x_train, t_train = x_train[:5000], t_train[:5000]
+#x_test, t_test = x_test[:1000], t_test[:1000]
+
+max_epochs = 20
+
+network = SimpleConvNet(input_dim=(1, 28, 28), 
+                        conv_param={'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+                        hidden_size=100, output_size=10, weight_init_std=0.01)
+
+trainer = Trainer(network, x_train, t_train, x_test, t_test,
+                  epochs=max_epochs, mini_batch_size=100,
+                  optimizer='Adam', optimizer_param={'lr': 0.001},
+                  evaluate_sample_num_per_epoch=1000)
+trainer.train()
+
+# Save parameters
+network.save_params("params.pkl")
+print("Saved Network Parameters!")
+
+# Plot graph
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(max_epochs)
+plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
+
+"""
+6.6 Visualize CNN
+"""
